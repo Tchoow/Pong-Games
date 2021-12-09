7853f145636d60367863ab4352ec2afd26b5ce6e
Theo Lavie <theo.l@studiosport.fr>
2021-12-09 15:42:43 +0100

    init canvas

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const shareBtn = document.querySelector(".share");
 const bar1     = document.querySelector(".bar");
-
+var canvas = document.querySelector("canvas");
 var lig, col = 0;
 
 function shareLink() {
   navigator.clipboard.writeText("https://codepen.io/Tchoow/full/BadWgEZ");
   shareBtn.innerText = "Lien copié !";
 }
+
